a19b7a4f8659b73a9c9a72bf1d83a30c548e853e
Luis Alberto Segura Gavilan <luis.alberto.segura.gavilan@unit4.com>
2021-12-16 14:21:40 +0100

    Add project files.

---
+++ PubSub/Program.cs
@@ -0,0 +1,42 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Threading.Tasks;
+
+namespace Send
+{
+    public class Program
+    {
+        private const string ConnectionString = "";
+        private const string QueueName = "";
+        private const int NumberOfMessages = 100;
+
+        public static async Task Main()
+        {
+            ServiceBusClient serviceBusClient = new ServiceBusClient(ConnectionString);
+            ServiceBusSender sender = serviceBusClient.CreateSender(QueueName);
+
+            using ServiceBusMessageBatch messageBatch = await sender.CreateMessageBatchAsync();
+            for (int i = 1; i <= NumberOfMessages; i++)
+            {
+                if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+                {
+                    throw new Exception($"The message {i} is too large to fit in the batch.");
+                }
+            }
+
+            try
+            {
+                await sender.SendMessagesAsync(messageBatch);
+                Console.WriteLine($"A batch of {NumberOfMessages} messages has been published to the queue {QueueName}.");
+            }
+            finally
+            {
+                await sender.DisposeAsync();
+                await serviceBusClient.DisposeAsync();
+            }
+
+            Console.WriteLine("Press any key to end the application");
+            Console.ReadKey();
+        }
+    }
+}
